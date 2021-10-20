d166af3fc7577cdf29cb9f06aaaa13f8121ac1d3
Alaa Yacoub <41425443+alaaelbarasi@users.noreply.github.com>
2021-10-20 12:47:31 +0200

    creating feature columns and input function

--- flower_classification.py
+++ flower_classification.py
@@ -21,3 +21,18 @@ test=pd.read_csv(test_path,names=cols_names,header=0)
 # poping out species because it's the one we want to predicit
 y_train=train.pop('Species')
 y_test=test.pop('Species')
+
+#input function the trainig process
+def input_fn(features, labels, trainig=True,batch_zise=256):
+    #The batch size is a number of samples processed before the model is updated
+    ##The number of epochs is the number of complete passes through the training dataset
+    ###Convert the input into dataset
+    ds=tf.data.Dataset.from_tensor_slices((dict(features),labels))
+    if trainig:
+        ds=ds.shuffle(1000).repeat()
+    return ds.batch(batch_zise)
+# creating feature columns
+feature_col=[]
+for key in train.keys():
+     feature_col.append(tf.feature_columns.numeric_column(key=key))
+
