8ea3ee1b17581f5ab087ca5fd26e08550f06a117
Alaa Yacoub <41425443+alaaelbarasi@users.noreply.github.com>
2021-10-20 12:15:18 +0200

    popping out the desired column

--- flower_classification.py
+++ flower_classification.py
@@ -2,8 +2,14 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 import pandas as pd 
 
-cols_names=['sepalLength','sepalWidth','petalLength','petalWidth']
+cols_names=['sepalLength','sepalWidth','petalLength','petalWidth','Species']
 species=['Setosa','Versicolor','Virginica']# classes the datapoints belong to
+
+# sepcies
+# 0=setosa
+#1=Versicolor
+#2=Virginica
+
 #uploading data frame
 train_path=tf.keras.utils.get_file('train.csv','https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv')
 test_path=tf.keras.utils.get_file('test.csv','https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv')
@@ -11,4 +17,7 @@ test_path=tf.keras.utils.get_file('test.csv','https://storage.googleapis.com/dow
 #reading data frame
 train=pd.read_csv(train_path,names=cols_names,header=0)
 test=pd.read_csv(test_path,names=cols_names,header=0)
-print(train.head())
+#all the stored data has numerical values.
+# poping out species because it's the one we want to predicit
+y_train=train.pop('Species')
+y_test=test.pop('Species')
