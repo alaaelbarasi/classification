ed94a5994d44e675818529773aafc1822d5b5a5b
Alaa Yacoub <41425443+alaaelbarasi@users.noreply.github.com>
2021-10-20 12:04:01 +0200

    solved the error

--- flower_classification.py
+++ flower_classification.py
@@ -1,12 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import pandas as pd 
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 cols_names=['sepalLength','sepalWidth','petalLength','petalWidth']
 species=['Setosa','Versicolor','Virginica']# classes the datapoints belong to
 #uploading data frame
-train_path=tf.kares.utils.get_file('train.csv','https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv')
-test_path=tf.kares.utils.get_file('test.csv','https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv"')
+train_path=tf.keras.utils.get_file('train.csv','https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv')
+test_path=tf.keras.utils.get_file('test.csv','https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv')
+
 #reading data frame
-train=pd.read_csv('train_path',names=cols_names,header=0)
-test=pd.read_csv('test_path',names=cols_names,header=0)
+train=pd.read_csv(train_path,names=cols_names,header=0)
+test=pd.read_csv(test_path,names=cols_names,header=0)
+print(train.head())
