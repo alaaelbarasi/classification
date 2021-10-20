fef5a6cc3ffb4510c1e1808feab7fc5897f32783
Alaa Yacoub <41425443+alaaelbarasi@users.noreply.github.com>
2021-10-20 11:49:42 +0200

    uploading and reading data frames

--- flower_classification.py
+++ flower_classification.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
 import pandas as pd 
-import numpy as np
+from __future__ import absolute_import, division, print_function, unicode_literals
 
+cols_names=['sepalLength','sepalWidth','petalLength','petalWidth']
+species=['Setosa','Versicolor','Virginica']# classes the datapoints belong to
+#uploading data frame
+train_path=tf.kares.utils.get_file('train.csv','https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv')
+test_path=tf.kares.utils.get_file('test.csv','https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv"')
+#reading data frame
+train=pd.read_csv('train_path',names=cols_names,header=0)
+test=pd.read_csv('test_path',names=cols_names,header=0)
